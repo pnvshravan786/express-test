19f606caaf9e1519a7aa7159e36c9d61a77be785
Naga Venkata Shravan Proddatur <pnvshravan@heinwig.com>
2025-03-26 22:59:37 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// import express from "express";
+// import os from "os";
+
+// const containerId = os.hostname();
+
+// const app = express();
+// const PORT = 5000;
+
+// app.get('/', (req,res)=>{
+//     const headers = req.headers;
+//     res.send(`Hello from the backend server running in container ID: ${containerId}`);
+// });
+
+// app.get('/header', (req,res)=>{
+//     const headers = req.headers;
+//     res.send(headers);
+// });
+
+// app.listen(PORT, () =>{
+//     console.log(`Express listening on port ${PORT}`);
+// })
+
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000; // Ensure it uses 3000
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Express listening on port ${PORT}`);
+});
