a119783f0c6ea8df49c85e7740b8e54eb17b8195
pnvshravan786 <pnvshravan@heinwig.com>
2025-03-29 14:40:49 +0530

    test trigger-i3

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 export default {
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],  // ✅ Treat .js files as ESM
   testEnvironment: 'node',         // ✅ Ensure Jest runs in a Node.js environment
 };
